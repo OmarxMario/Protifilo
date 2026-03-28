2ddcb437f2d07acb257c93785821196675657d41
OmarxMario <156704505+OmarxMario@users.noreply.github.com>
2026-03-28 21:47:52 +0200

    Create animation_engine.js

---
+++ js/animation_engine.js
@@ -0,0 +1,14 @@
+const intersectionObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show'); 
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden'); // Select all elements with class 'hidden'
+hiddenElements.forEach(element => {
+    intersectionObserver.observe(element); // Observe each element
+});
